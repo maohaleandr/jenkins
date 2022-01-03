fa567f2b0331dffc940745a48e7d8de48d4e74dd
maohaleandr <maohalejandr@gmail.com>
2022-01-03 11:18:03 -0600

    consultaCreditoSears initial commit

---
+++ consultaCreditoSears/index.js
@@ -0,0 +1,37 @@
+const { exit } = require('process');
+const newman = require('C:/Users/ksolis/AppData/Roaming/npm/node_modules/newman');
+var code = "";
+var results = [];
+
+
+newman.run({
+    collection: require('./pruebaCredito.postman_collection.json'),
+    environment: './Sears.postman_environment.json',
+    reporters: 'html',
+    reporter: {
+        html: {
+            export: './newmanResults/resultsCreditoSears.html',
+        }
+    }
+}, function (err, res) {
+	if (err) { throw err; }
+    console.log('collection Api Credito sears run complete!');
+})
+.on('request', function (err, args) {
+    code = args.response.code;
+    results = JSON.parse(args.response.stream.toString());
+})
+.on('done', function (err, args) {
+    if(code == '200'){
+        if(results[0].name == "LUIS E"){
+            console.log("RESPUESTA EXITOSA");
+        }
+        else{
+            console.log("---LA RESPUESTA DEL REQUEST NO ES LA ESPERADA; EL NOMBRE NO COINCIDE: " + results[0].name);
+            process.abort();
+        }
+    }else{
+        console.log("---LA RESPUESTA DEL REQUEST NO ES LA ESPERADA; CODIGO: " + code);
+        process.abort();
+    }
+});
